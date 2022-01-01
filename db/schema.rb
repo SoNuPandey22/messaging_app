a1e3cf7fa54db3a8080b062b468dd149c3332c16
Sonu Pandey <sonu.pandey@4moles.com>
2022-01-01 23:36:43 +0530

    Message shoing dynamically and renderd partial with naming convention

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220101112411) do
+ActiveRecord::Schema.define(version: 20220101173149) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
