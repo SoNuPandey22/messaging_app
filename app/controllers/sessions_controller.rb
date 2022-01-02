89e7a505afc759763c33321c0e97f3da4bf1234d
Sonu Pandey <sonu.pandey@4moles.com>
2022-01-02 10:40:10 +0530

    user authenticated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,24 @@
 class SessionsController < ApplicationController
   def new 
+
   end 
   
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Loged in successfully"
+      redirect_to root_path
+    else
+      flash.now[:error] = "User not found with these credentials"
+      render "new"
+    end
+  end 
+
+  def destroy 
+     session[:user_id] = nil
+     flash[:success] = "successfully log off"
+     redirect_to login_path
+  end 
+
 end 
